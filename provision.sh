42edc6e2823414fdb0558b53ee483426a1eef40c
kris-o3 <kris@o3world.com>
2015-01-30 09:18:35 -0500

    add Redis, php-redis; add database backup/restore to provision (retain 15-minute old data after 'destroy' and 'up')

--- provision.sh
+++ provision.sh
@@ -54,13 +54,30 @@ sed -i 's/^post_max_size.*/post_max_size = 160M/' php.ini
 apt-get install -y mysql-client-5.6 mysql-server-5.6
 
 cd /etc/mysql
-sed -i '/^\[mysqld\].*/a explicit_defaults_for_timestamp = 1' my.cnf
+sed -i '/^\[mysqld\].*/aexplicit_defaults_for_timestamp = 1' my.cnf
 sed -i 's/^key_buffer[^_]/key_buffer_size/' my.cnf
 sed -i 's/^max_allowed_packet.*/max_allowed_packet = 128M/' my.cnf
 sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' my.cnf
 
 mysql -e "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION; UPDATE mysql.user SET Password = PASSWORD('vagrant') WHERE User='root'; FLUSH PRIVILEGES;"
 mysqladmin -uroot -pvagrant shutdown
+
+if [ -f /sync/.mysql.tgz ]
+then
+	cd /
+	rm -rf /var/lib/mysql
+	tar xvfp /sync/.mysql.tgz
+fi
+
+(crontab -l ; echo "*/15 * * * * tar cvfp /sync/.mysql.tgz /var/lib/mysql > /dev/null 2>&1") | crontab -
+
+
+# ---- redis, php-redis
+
+apt-get install -y redis-server php5-redis
+
+cd /etc/redis
+sed -i 's/^bind.*/bind 0.0.0.0/' redis.conf
 
 
 # ---- nginx
