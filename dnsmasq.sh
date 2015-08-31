213ad3ffffc07654c65ab6c741cffeb378b54ab8
kris@o3world.com <kris@o3world.com>
2015-08-31 16:50:59 -0400

    limit dnsmasq resolution to .local.dev rather than .dev

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -2,7 +2,7 @@
 
 brew install dnsmasq
 mkdir -pv $(brew --prefix)/etc/
-echo 'address=/.dev/192.168.200.2' > $(brew --prefix)/etc/dnsmasq.conf
+echo 'address=/.local.dev/192.168.200.2' > $(brew --prefix)/etc/dnsmasq.conf
 sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons
 sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
 sudo mkdir -v /etc/resolver
