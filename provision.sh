81cc8495ef70bf9cff639ba495a87a524d2fd56d
Courtney Wilburn <courtney@cjwilburn.com>
2015-09-09 09:46:48 -0400

    adding provisioning for wkhtmltopdf on local box

--- provision.sh
+++ provision.sh
@@ -93,6 +93,14 @@ mv /tmp/nginx.conf /etc/nginx/nginx.conf
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" nginx
 
 
+# ---- wkhtmltopdf
+apt-get install wkhtmltopdf
+apt-get install xvfb
+echo 'xvfb-run --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf $*' > /usr/bin/wkhtmltopdf.sh
+chmod a+rx /usr/bin/wkhtmltopdf.sh
+ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
+# -- wkhtmltopdf http://www.google.com output.pdf
+
 # ---- post-provision cleanup
 
 apt-get clean
